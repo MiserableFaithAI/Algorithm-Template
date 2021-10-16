a28b4f7c9cc82779295fdd59c76a10a706372d26
Henry <52558285+MiserableFaithAI@users.noreply.github.com>
2021-10-16 20:06:47 +0800

    finish heap.c and add some description to README.md

--- Basic Heap/heap.c
+++ Basic Heap/heap.c
@@ -1,6 +1,47 @@
-#include <stdio.h>
+int HEAP[100];
+int SIZE = 0;
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+int put(int number) {
+    /**
+     * @return location of the inserted
+     */
+    HEAP[SIZE] = number;
+    int now = SIZE - 1;
+    while (now > 1) {
+        int next = now / 2;
+        if (HEAP[next - 1] > HEAP[now - 1]) {
+            int tmp = HEAP[next - 1];
+            HEAP[next - 1] = HEAP[now - 1];
+            HEAP[now - 1] = tmp;
+            now /= 2;
+        } else {
+            break;
+        }
+    }
+    return now;
+}
+
+int pop() {
+    int result = HEAP[0];
+    HEAP[0] = HEAP[-- SIZE];
+    int now = 1;
+    while (now * 2 <= SIZE) {
+        int next = 2 * now;
+        if (HEAP[next] < HEAP[next - 1]) { // 这里注意选择更小的子节点
+            next ++;
+        }
+        if (HEAP[next - 1] < HEAP[now - 1]) {
+            int tmp = HEAP[next - 1];
+            HEAP[next - 1] = HEAP[now - 1];
+            HEAP[now - 1] = tmp;
+            now = next;
+        } else {
+            break;
+        }
+    }
+    return result;
+}
+
+int top() {
+    return HEAP[0];
 }
